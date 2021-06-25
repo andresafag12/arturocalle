c6a5f87619893c5c23c7dfd822e58e8a1ccbc038
Andres Acosta <danielgarciaacosta46qgmail.com>
2021-06-25 09:38:21 -0500

    arturito

---
+++ ArturoCalle/js.js
@@ -0,0 +1,15 @@
+const para = document.querySelector('.text-box');
+
+function chaneText() {
+    let counter = 0
+
+    const frases = new Array
+
+    frases[0] = SERVICIO ENTREGA EXPRESSS PARA BOGOTA
+    frases[1] = CONOCE AQUI TODOS LOS PROTOCOLOS DE SEGURIDAD EN TIENDAS AC
+    frases[2] = DEBIDO A LA SITUACION DE ORDEN PUBLICO NUESTROS TIEMPOS DE ENTREGA PUEDEN VERSE AFECTADOS
+    frases[3] = ¿PODEMOS AYUDARTE ? REGISTRA REGISTRA AQUI TUSS PQRS
+
+    for (let i = 0;)
+
+}
